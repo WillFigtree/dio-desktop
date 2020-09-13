2c2b816ac819955c0a67b52eb4a75706ed95b0ee
WillFigtree <w.figtree@gmail.com>
2020-09-13 15:07:11 +1000

    Started on tx/rx command pattern

--- DioCli/Program.cs
+++ DioCli/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.IO;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace DioCli
 {
     class Program
     {
-        static void Main(string[] args)
+        static readonly int BlinkInterval = 200;    // the interval between LED blinks
+        static readonly int PollInterval = 10;      // rx polling interval (milliseconds)
+        static readonly int RxBufferSize = 1024;    // rx buffer size (bytes)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("====Dio CLI====");
 
@@ -33,18 +37,74 @@ namespace DioCli
                 return;
             }
 
-            // Read for a while
-            var rxBuffer = new byte[1024];
-            for (int i = 0; i < 1000; i++)
+            var cts = new CancellationTokenSource(5000);
+            var rxTask = RecieveLoopAsync(port, cts.Token);
+            var txTask = TransmitLoopAsync(port, cts.Token);
+
+            try
             {
-                Thread.Sleep(10);
-                var n = RS232.ReadPort(port, rxBuffer);
-                var s = System.Text.Encoding.ASCII.GetString(rxBuffer, 0, n);
-                Console.Write(s);
+                await Task.WhenAll(rxTask, txTask);
+            }
+            catch (OperationCanceledException) { }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occured during rx/tx: {ex.Message}");
+            }
+            finally
+            {
+                // Close the port
+                RS232.ClosePort(port);
             }
 
-            // Close the port
-            RS232.ClosePort(port);
+
+            //// Read for a while
+            //var rxBuffer = new byte[1024];
+            //for (int i = 0; i < 1000; i++)
+            //{
+            //    Thread.Sleep(10);
+            //    var n = RS232.Read(port, rxBuffer);
+            //    var s = System.Text.Encoding.ASCII.GetString(rxBuffer, 0, n);
+            //    Console.Write(s);
+            //}
+
+            //// Close the port
+            //RS232.ClosePort(port);
+
+            Console.WriteLine("DIO complete.");
+        }
+
+        static async Task TransmitLoopAsync(int portNum, CancellationToken ct)
+        {
+            ct.ThrowIfCancellationRequested();
+            byte[] bytes;
+
+            // transmit led on/off commands 
+            while(true)
+            {
+                bytes = System.Text.Encoding.ASCII.GetBytes("LED ON");
+                RS232.Write(portNum, bytes);
+                await Task.Delay(BlinkInterval / 2, ct).ConfigureAwait(false);
+
+                bytes = System.Text.Encoding.ASCII.GetBytes("LED OFF");
+                RS232.Write(portNum, bytes);
+                await Task.Delay(BlinkInterval / 2, ct).ConfigureAwait(false);
+            }
+        }
+
+        static async Task RecieveLoopAsync(int portNum, CancellationToken ct)
+        {
+            ct.ThrowIfCancellationRequested();
+
+            var rxBuffer = new byte[RxBufferSize];
+            while(true)
+            {
+                await Task.Delay(PollInterval, ct).ConfigureAwait(false);
+
+                var n = RS232.Read(portNum, rxBuffer);
+                var s = System.Text.Encoding.ASCII.GetString(rxBuffer, 0, n);
+
+                Console.Write(s);
+            }
         }
     }
 }
